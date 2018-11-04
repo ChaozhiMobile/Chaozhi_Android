9d1911e76b385eab57d2983c2700a66825a128b8
hanzhanbing <1655661337@qq.com>
2018-11-04 11:24:31 +0800

    关注课程图标添加

--- app/src/main/java/com/czjy/chaozhi/ui/adapter/SubjectAdapter.java
+++ app/src/main/java/com/czjy/chaozhi/ui/adapter/SubjectAdapter.java
@@ -1,11 +1,13 @@
 package com.czjy.chaozhi.ui.adapter;
 
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.czjy.chaozhi.R;
 import com.czjy.chaozhi.model.response.SubjectsResponse;
+import com.czjy.chaozhi.util.glide.CommonGlideImageLoader;
 
 import java.util.List;
 
@@ -23,12 +25,14 @@ public class SubjectAdapter extends BaseQuickAdapter<SubjectsResponse,BaseViewHo
     @Override
     protected void convert(BaseViewHolder helper, SubjectsResponse item) {
         helper.setText(R.id.item_subject_name,item.getName());
+        ImageView iconImgView = helper.getView(R.id.item_subject_icon);
+        CommonGlideImageLoader.getInstance().displayNetImage(mContext,item.getImg(),iconImgView,mContext.getResources().getDrawable(R.drawable.default_square_img));
         if (item.isSelect()){
             helper.setVisible(R.id.item_subject_img,true);
-            helper.setTextColor(R.id.item_subject_name,mContext.getResources().getColor(R.color._C31A1F));
+//            helper.setTextColor(R.id.item_subject_name,mContext.getResources().getColor(R.color._C31A1F));
         }else{
             helper.setVisible(R.id.item_subject_img,false);
-            helper.setTextColor(R.id.item_subject_name,mContext.getResources().getColor(R.color._24253D));
+//            helper.setTextColor(R.id.item_subject_name,mContext.getResources().getColor(R.color._24253D));
         }
 
     }
