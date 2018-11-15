b1aa51fbaf1caca7a08d35910350138ee80b1344
hanzhanbing <1655661337@qq.com>
2018-11-15 17:22:08 +0800

    重新登录逻辑添加

--- app/src/main/java/com/czjy/chaozhi/ui/activity/MainActivity.java
+++ app/src/main/java/com/czjy/chaozhi/ui/activity/MainActivity.java
@@ -248,7 +248,7 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
         }
 
         if (name.isEmpty()) {
-            name = "1.0.0";
+            name = "1.1.0";
         }
         mPresenter.checkVersion("android", name);
     }
